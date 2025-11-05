7228da9f390ae21171c05cb9b60173d8370b12c7
Edith Heidmann <eheidmann@gmail.com>
2025-11-05 11:34:00 +0100

    add header, update layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import HomeView from "./views/HomeView.vue";
 </script>
 
 <template>
+  <Header />
   <main class="my-6 mx-auto max-w-3xl">
-    <HelloWorld />
+    <!-- // todo: add some routes for fun and demo - maybe a 'done' view -->
+    <HomeView />
   </main>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header
+    class="flex justify-center bg-[var(--background-color-secondary)] py-4"
+  >
+    <h1 class="flex h-14 items-center text-3xl">To do List</h1>
+  </header>
+</template>
