49af4f8f25f5d8b8399cba73e3871c20e6ac8899
Edith Heidmann <eheidmann@gmail.com>
2025-11-06 19:01:56 +0100

    group tasks to map and remove duplicate table body, add time as well as date

--- src/components/TasksTable.vue
+++ src/components/TasksTable.vue
@@ -4,13 +4,10 @@ import ActionMenu from "./ActionMenu.vue";
 import { computed } from "vue";
 const taskStore = useTaskStore();
 
-const outstandingTasks = computed(() =>
-  taskStore.tasks.filter((task) => !task.status)
-);
-
-const statusTasks = computed(() =>
-  taskStore.tasks.filter((task) => task.status)
-);
+const groupedTasks = computed(() => [
+  { title: "To do", tasks: taskStore.tasks.filter((t) => !t.status) },
+  { title: "Done", tasks: taskStore.tasks.filter((t) => t.status) },
+]);
 </script>
 <template>
   <!-- // todo: style 'todo' and 'done' sections to look nicer  -->
@@ -35,43 +32,35 @@ const statusTasks = computed(() =>
           </th>
         </tr>
       </thead>
-
       <tbody class="bg-white divide-y divide-gray-200">
-        <tr class="font-semibold uppercase">
-          To do
-        </tr>
-        <tr
-          v-for="task in outstandingTasks"
-          :key="task.id"
-          class="hover:bg-gray-50"
-        >
-          <td class="px-6 py-4 border-r border-gray-200">
-            {{ new Date(task.date).toLocaleDateString() }}
-          </td>
-          <td class="px-6 py-4 border-r border-gray-200 text-sm w-[300px]">
-            {{ task.description }}
-          </td>
-          <td class="px-6 py-4 text-right">
-            <ActionMenu :taskId="task.id" />
-          </td>
-        </tr>
-      </tbody>
-
-      <tbody class="bg-white divide-y divide-gray-200">
-        <tr class="font-semibold uppercase">
-          Done
-        </tr>
-        <tr v-for="task in statusTasks" :key="task.id" class="hover:bg-gray-50">
-          <td class="px-6 py-4 border-r border-gray-200">
-            {{ new Date(task.date).toLocaleDateString() }}
-          </td>
-          <td class="px-6 py-4 border-r border-gray-200 text-sm w-[300px]">
-            {{ task.description }}
-          </td>
-          <td class="px-6 py-4 text-right">
-            <ActionMenu :taskId="task.id" />
-          </td>
-        </tr>
+        <template v-for="group in groupedTasks" :key="group.title">
+          <tr class="font-semibold uppercase bg-gray-100">
+            <td colspan="3" class="px-6 py-2">{{ group.title }}</td>
+          </tr>
+          <tr
+            v-for="task in group.tasks"
+            :key="task.id"
+            class="hover:bg-gray-50"
+          >
+            <td class="px-6 py-4 border-r border-gray-200">
+              {{
+                new Date(task.date).toLocaleString(undefined, {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })
+              }}
+            </td>
+            <td class="px-6 py-4 border-r border-gray-200 text-sm w-[300px]">
+              {{ task.description }}
+            </td>
+            <td class="px-6 py-4 text-right">
+              <ActionMenu :taskId="task.id" />
+            </td>
+          </tr>
+        </template>
       </tbody>
     </table>
   </div>
